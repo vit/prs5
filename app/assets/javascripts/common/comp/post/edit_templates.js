6ce92584fa034fc270e9a30a9dae2fc0e94dd0f4
vit <none@none>
2012-08-11 21:15:53 +0000

    m

--- app/assets/javascripts/common/comp/post/edit_templates.js
+++ app/assets/javascripts/common/comp/post/edit_templates.js
@@ -54,9 +54,9 @@ window.addEvent('domready',function() {
 			tList.reload();
 			panel.grab(tList.panel);
 
-/*
+///*
 			var oneT = new Coms.Comp.Post.EditOneTemplate({});
-			oneT.show(false);
+		//	oneT.show(false);
 			oneT.attach('close_item', function(arg) {
 				oneT.show(false);
 				List.show(true);
@@ -64,7 +64,7 @@ window.addEvent('domready',function() {
 			//	new_thread_draft_btn_show(true);
 			});
 			panel.grab(oneT.panel);
-*/
+//*/
 
 			return me;
 		};
@@ -122,6 +122,104 @@ window.addEvent('domready',function() {
 			Mixin.implement(me, Mixin.Observable);
 			return me;
 		}
+	}());
+
+	/* TemplatePanel -- Create, save, edit or delete single template */
+//	self.Coms.Comp.Post.TemplatePanel = (function() {
+	self.Coms.Comp.Post.EditOneTemplate = (function() {
+		return function(args, msg_id, thread_id) {
+			var user = window.user;
+		//	var user = window.user;
+			var panel = new Element('div', { class: '' });
+			var cont = new Element('div', {styles: {ddisplay: 'none'}});
+			panel.grab(cont);
+			var thread_title_label = new Element('b', {text: 'Topic title '});
+			var thread_title = new Element('input', {type: 'text'});
+			var msg_text_label = new Element('b', {text: 'Message text'});
+			var msg_text = new Element('textarea', {styles: {
+				width: '40em',
+				height: '6em'
+			}});
+			var add_msg_btn = new Element('input', {type: 'button', class: '', value: 'Send message', events: {
+				click: function(e){
+					RPC.send('msg.save_my_message_draft_data', [user.id, msg_id, {msg_text: msg_text.get('value'), thread_title: thread_title.get('value')}], function(result, error) {
+						RPC.send('msg.save_my_draft_as_message', [user.id, msg_id], function(result, error) {
+					//		alert(JSON.encode(result));
+							me.notify('message_saved', msg_id);
+							me.panel.dispose();
+						});
+					});
+				}
+			}});
+			var save_draft_btn = new Element('input', {type: 'button', class: '', value: 'Save draft', events: {
+				click: function(e){
+				//	alert( msg_id );
+					RPC.send('msg.save_my_message_draft_data', [user.id, msg_id, {msg_text: msg_text.get('value'), thread_title: thread_title.get('value')}], function(result, error) {
+				//		alert(JSON.encode(result));
+					});
+				}
+			}});
+			var delete_msg_btn = new Element('input', {type: 'button', class: '', value: 'Delete draft', events: {
+				click: function(e){
+					clear_form();
+					RPC.send('msg.delete_my_message_draft', [user.id, msg_id], function(result, error) {
+				//		alert(JSON.encode(result));
+				//		me.notify('deleted', msg_id);
+						me.panel.dispose();
+					});
+				//	cont.setStyles({display: 'none'});
+				}
+			}});
+			if( !thread_id ) {
+				cont.adopt(
+					thread_title_label,
+					thread_title,
+					new Element('br')
+				);
+			}
+			cont.adopt(
+				msg_text_label,
+				new Element('br'),
+				msg_text,
+				new Element('br'),
+				add_msg_btn,
+				save_draft_btn,
+				delete_msg_btn
+			);
+
+			/*
+			if( msg_id ) {
+				RPC.send('msg.get_my_message_draft_data', [user.id, msg_id], function(result, error) {
+				//	alert(JSON.encode(result));
+					if(result) {
+						msg_text.set('value', result.msg_text);
+						thread_title.set('value', result.thread_title);
+					}
+				});
+			} else {
+			//	RPC.send('msg.create_my_message_draft_on_paper', [user.id, args.cont_id, args.paper_id, null], function(result, error) {
+				RPC.send('msg.create_my_message_draft_on_paper', [user.id, args.cont_id, args.paper_id, thread_id], function(result, error) {
+				//	alert(JSON.encode(result));
+					if(result) {
+						msg_id = result._id;
+					}
+				});
+			}
+
+			function clear_form() {
+				thread_title.set('value', '');
+				msg_text.set('value', '');
+			}
+			*/
+
+			var me = {
+				panel: panel //,
+			//	reload: reload,
+			//	show: function (flag) {panel.setStyle('display', flag ? 'block' : 'none')}
+			};
+			Mixin.implement(me, Mixin.Observable);
+			return me;
+		};
 	}());
 
 
