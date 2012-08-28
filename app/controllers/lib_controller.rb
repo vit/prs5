80bd6b010cfb8a54ca7c5661c97b14e78af5e812
vit <none@none>
2012-08-28 21:37:30 +0000

    m

--- app/controllers/lib_controller.rb
+++ app/controllers/lib_controller.rb
@@ -8,13 +8,29 @@ class LibController < ApplicationController
 #	render :text => 'r rt re trt rt wet wer wffqr'
   end
   def conferences
+	id = Coms::App.model.lib.get_id_by_alias 'conferences'
+	item id
+
 	#render text: 'wwwww'
-#	render text: params.to_s
+	#render text: params.to_s
+	#render text: id
 	#render text: lib_query
   end
   def periodicals
   end
   def books
+  end
+  def item id=nil
+	id = params['id'] unless id
+#	@children = []
+	@data = Coms::App.model.lib.get_item_data id
+	@children = Coms::App.model.lib.get_item_children id
+#	if @data then
+#		if @data['type']=='CONF' then
+#		end
+#	end
+	#render text: 'wwwww'
+	render :file => 'lib/conferences'
   end
 end
 
