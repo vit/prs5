93c671b378481f65dfcf1b9762735d41ed510c41
vit <none@none>
2012-11-19 20:18:28 +0000

    m

--- app/assets/javascripts/comp/conf/participation/form.js
+++ app/assets/javascripts/comp/conf/participation/form.js
@@ -1,6 +1,8 @@
 
 'use strict'
 
+
+
 var ParticipationForm = function() {
 	return (function($) {
 		var formStruct = [
@@ -35,33 +37,48 @@ var ParticipationForm = function() {
 			{name: 'publish_agree', type: 'radio'}
 		//	{name: '', type: 'text'},
 		];
-		function getElemData(cont, v) {
-			var elm = cont.find('[name='+v.name+']');
-			var len = elm.length;
-			var rez;
-			if( len > 1 ) {
-				rez = null;
-				$.each(elm, function(k, v) {
-					v = $(v);
-					if( v.attr('checked') ) {
-						rez = v.val();
-					}
+		var FormAccess = function( cont, info ) {
+			function initForm() {
+				$.each(formStruct, function(k,v) {
+					if(v.type=='date')
+						cont.find('[name='+v.name+']').datepicker({
+							"dateFormat": 'yy-mm-dd'
+						});
 				});
-			} else {
-				rez = elm.val();
-				if( v.type=='checkbox' ) {
-					rez = elm.attr('checked') ? true : false;
-				}
 			}
-			//var rez = elm.length;
-			return rez;
-		}
-		function getData() {
-			var rez = {};
-			$.each(formStruct, function(k,v) {
-				rez[v.name] = getElemData(cont, v);
-			});
-			return rez;
+			function getElemData(cont, v) {
+				var elm = cont.find('[name='+v.name+']');
+				var len = elm.length;
+				var rez;
+				if( len > 1 ) {
+					rez = null;
+					$.each(elm, function(k, v) {
+						v = $(v);
+						if( v.attr('checked') ) {
+							rez = v.val();
+						}
+					});
+				} else {
+					rez = elm.val();
+					if( v.type=='checkbox' ) {
+						rez = elm.attr('checked') ? true : false;
+					}
+				}
+				//var rez = elm.length;
+				return rez;
+			}
+			function getData() {
+				var rez = {};
+				$.each(info, function(k,v) {
+					rez[v.name] = getElemData(cont, v);
+				});
+				return rez;
+			}
+			var me = {
+				get: getData
+			};
+			initForm();
+			return me;
 		}
 
 		var me = {
@@ -69,12 +86,15 @@ var ParticipationForm = function() {
 		};
 
 		var cont = $('#participationcomp');
-		$.each(formStruct, function(k,v) {
-			if(v.type=='date')
-				cont.find('[name='+v.name+']').datepicker({
-					"dateFormat": 'yy-mm-dd'
-				});
-		});
+		var form = new FormAccess( cont, formStruct );
+
+//		$.each(formStruct, function(k,v) {
+//			if(v.type=='date')
+//				cont.find('[name='+v.name+']').datepicker({
+//					"dateFormat": 'yy-mm-dd'
+//				});
+//		});
+
 //		$( "#form_birthdate", cont ).datepicker({
 //			"dateFormat": 'yy-mm-dd'
 //		});
@@ -86,7 +106,8 @@ var ParticipationForm = function() {
 
 
 		$( "#form_save_btn", cont ).click(function(){
-			var data = getData();
+		//	var data = getData();
+			var data = form.get();
 			alert(JSON.encode(data));
 		});
 
