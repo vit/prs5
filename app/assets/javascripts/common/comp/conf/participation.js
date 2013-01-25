2ae4b80343fdfac8c3e807292bfd7f845152de82
vit <none@none>
2013-01-25 22:49:29 +0000

    m

--- app/assets/javascripts/common/comp/conf/participation.js
+++ app/assets/javascripts/common/comp/conf/participation.js
@@ -32,7 +32,8 @@
 		var form_on_elm = $('[name=form_on]', cont);
 		var form_off_elm = $('[name=form_off]', cont);
 
-		var form_on = ParticipationFormOn( form_on_elm );
+	//	var form_on = ParticipationFormOn( form_on_elm );
+		var form_on = ParticipationFormOn2( form_on_elm );
 		var form_off = ParticipationFormOff( form_off_elm );
 
 		form_off.attach('want_register', function() {
@@ -74,6 +75,101 @@
 
 		return me;
 	};
+
+
+
+	var ParticipationFormOn2 = function(cont) {
+		var dict = new Dict( $('[name=dict]', cont) );
+		var form = $('form', cont);
+		function rulesCoauthors(name, err) {
+			if( this.get()['part_type'] != 'nonauthor' ) {
+				err('required');
+			}
+		}
+		function rulesDegree(name, err) {
+			if( this.get()['occupation'] == 'work' ) {
+				err('required');
+			}
+		}
+		function goOccupationDependent(name) {
+	//		if( this.get()['occupation'] == 'work' ) {
+				this.setEnabled('position', this.get()['occupation'] == 'work');
+			//	err('required');
+	//		}
+		}
+
+		var formStruct = [
+			{name: 'gender', type: 'radio', isvalid: 'notempty'},
+			{name: 'person_title', type: 'list', isvalid: 'notempty'},
+			{name: 'lname', type: 'text', isvalid: 'notempty'},
+			{name: 'fname', type: 'text', isvalid: 'notempty'},
+			{name: 'mname', type: 'text', isvalid: 'notempty'},
+			{name: 'birthdate', type: 'date', isvalid: 'notempty'},
+			{name: 'part_type', type: 'radio', isvalid: 'notempty'},
+			{name: 'coauthors', type: 'text', rules: rulesCoauthors},
+			{name: 'occupation', type: 'radio', isvalid: 'notempty', goDependent: goOccupationDependent},
+			{name: 'organization', type: 'text', isvalid: 'notempty'},
+			{name: 'position', type: 'text', rules: rulesDegree},
+			{name: 'rank', type: 'text', rules: rulesDegree},
+			{name: 'degree', type: 'text', rules: rulesDegree},
+			{name: 'responsibilities', type: 'text', isvalid: 'notempty'},
+			{name: 'org_country', type: 'text', isvalid: 'notempty'},
+			{name: 'org_city', type: 'text', isvalid: 'notempty'},
+			{name: 'org_postcode', type: 'text', isvalid: 'notempty'},
+			{name: 'org_street', type: 'text', isvalid: 'notempty'},
+			{name: 'org_house', type: 'text', isvalid: 'notempty'},
+			{name: 'org_foreign', type: 'checkbox'},
+			{name: 'phone', type: 'text', isvalid: 'notempty'},
+			{name: 'mobile_phone', type: 'text'},
+			{name: 'fax', type: 'text'},
+			{name: 'email', type: 'text', isvalid: 'notempty'},
+			{name: 'nationality', type: 'text', isvalid: 'notempty'},
+			{name: 'passport', type: 'text', isvalid: 'notempty'},
+			{name: 'hotel', type: 'radio', isvalid: 'notempty'},
+			{name: 'hotel_name', type: 'text'},
+			{name: 'publish_agree', type: 'radio', isvalid: 'notempty'}
+		//	{name: '', type: 'text'},
+		];
+
+		function init(data) {
+			$('form[name=unregister_form]', cont).css('display', data ? 'block' : 'none');
+		//	di.clear();
+			if(data)
+				di.set(data);
+		}
+
+		var me = {
+			panel: cont,
+			init: init
+		};
+
+		var di = new JSComp.FormAccess2( form, formStruct );
+
+		$( "[name=form_save_btn]", form ).click(function(){
+			if( di.validate() ) {
+				var data = di.get();
+				me.notify('save_form', data);
+			} else {
+				alert( dict('msg_missing_required_fields') );
+			}
+		});
+		$( "[name=unreg_btn]", form ).click(function(){
+			if( confirm(dict('unregister_are_you_sure')) ) {
+				me.notify('drop_form');
+			}
+		});
+
+		Mixin.implement(me, Mixin.Observable);
+		Mixin.implement(me, PanelMixin);
+		return me;
+	}
+
+/*
+
+
+
+
+
 
 	var ParticipationFormOn = function(cont) {
 	//	alert(cont.attr('name'));
@@ -139,8 +235,8 @@
 			init: init
 		};
 
-//		var cont = $('#participationcomp');
 		var di = new JSComp.FormAccess( form, formStruct );
+	//	var di = new JSComp.FormAccess2( form, formStruct );
 
 		$( "[name=form_save_btn]", form ).click(function(){
 		//	confirm(dict('unregister_are_you_sure'));
@@ -170,6 +266,7 @@
 	}
 
 
+*/
 
 	var ParticipationFormOff = function(cont) {
 		var form = $('form', cont);
@@ -198,13 +295,160 @@
 
 	self.JSComp = self.JSComp || {};
 
+
+
+		self.JSComp.FormAccess2 = function( cont, info ) {
+			var infoMap = {};
+			function initForm() {
+				$.each(info, function(k,v) {
+					infoMap[v.name] = v;
+					var elem = cont.find('[name='+v.name+']');
+					if(v.type=='date')
+						elem.datepicker({
+							"dateFormat": 'yy-mm-dd',
+							changeYear: true
+						});
+					if($.type(v.goDependent)=='function') {
+						elem.change(function() {
+					//		alert('changed!!!');
+							testOneField(v);
+						});
+					}
+				});
+				testAllFields();
+			}
+			function testOneField(v) {
+				if($.type(v.goDependent)=='function') {
+					v.goDependent.call(me, v);
+				}
+			}
+			function testAllFields() {
+				$.each(info, function(k,v) {
+					testOneField( v );
+				});
+			}
+			function unmarkAllElements() {
+				$.each(info, function(k,v) { markElement(v, false); });
+			}
+			function markElement(v, flag) {
+				var elm = cont.find('[name='+v.name+']');
+				var len = elm.length;
+				if( len > 1 ) {
+					elm.each(function() {
+						var p = $(this).parent();
+						flag ? p.addClass('marked') : p.removeClass('marked');
+					});
+				} else {
+					flag ? elm.addClass('marked') : elm.removeClass('marked');
+				}
+			}
+			function getElemData(v) {
+				var elm = cont.find('[name='+v.name+']');
+				var len = elm.length;
+				var rez;
+				if( len > 1 ) {
+					rez = null;
+					elm.each(function() {
+						v = $(this);
+						if( v.attr('checked') ) {
+							rez = v.val();
+						}
+					});
+				} else {
+					if( v.type=='checkbox' ) rez = elm.attr('checked') ? true : false;
+					else rez = elm.val();
+				}
+				//var rez = elm.length;
+				return rez;
+			}
+			function setElemData(v, d) {
+				var elm = cont.find('[name='+v.name+']');
+				var len = elm.length;
+				if( len > 1 ) {
+					elm.val([d]);
+				} else {
+					if( v.type=='checkbox' ) elm.attr('checked', d);
+					else elm.val(d);
+				}
+			}
+			function setEnabled(name, flag) {
+				var elm = cont.find('[name='+name+']');
+				elm.attr('disabled', !flag);
+			}
+			function getData() {
+				var rez = {};
+				$.each(info, function(k,v) {
+					rez[v.name] = getElemData(v);
+				});
+				return rez;
+			}
+			function setData(data) {
+			//	alert('set');
+				$.each(info, function(k,v) {
+					setElemData(v, data[v.name]);
+				});
+			}
+			function validate() {
+				var err = [];
+				unmarkAllElements();
+
+				$.each(info, function(k,v) {
+					if( v.rules ) {
+						var val = getElemData(v);
+		//				alert( $.type(v.rules) );
+						if( $.type(v.rules)=='string' ) {
+						//	var rules = v.rules.split('|');
+							var rules = v.isvalid.split('|');
+							$.each(rules, function(k2,v2) {
+								switch( v2 ) {
+								//	case 'required':
+									case 'notempty':
+										if(!val) {
+											markElement(v, true);
+											err.push({name: v.name, rule: v2});
+										}
+								}
+							});
+						} else if( $.type(v.rules)=='function' ) {
+						//	v.rules.call(me, v.name, err);
+							v.rules.call(me, v.name, function(rule) {
+								if(!val) {
+									markElement(v, true);
+									err.push({name: v.name, rule: rule});
+								}
+							});
+						}
+					}
+				});
+		//		alert( JSON.stringify(err) );
+				return err.length == 0;
+			}
+			var me = {
+				get: getData,
+				set: setData,
+				setEnabled: setEnabled,
+				validate: validate
+			};
+			initForm();
+			return me;
+		}
+
+
+
+
+/*
+
+
+
+
 		self.JSComp.FormAccess = function( cont, info ) {
 			function initForm() {
 			//	$.each(formStruct, function(k,v) {
 				$.each(info, function(k,v) {
 					if(v.type=='date')
 						cont.find('[name='+v.name+']').datepicker({
-							"dateFormat": 'yy-mm-dd'
+							"dateFormat": 'yy-mm-dd',
+							changeYear: true
 						});
 				});
 			}
@@ -261,7 +505,6 @@
 					if( v.type=='checkbox' ) elm.attr('checked', d);
 					else elm.val(d);
 				}
-			//	*/
 			}
 			function getData() {
 				var rez = {};
@@ -317,6 +560,8 @@
 			initForm();
 			return me;
 		}
+
+*/
 
 }(jQuery));
 
