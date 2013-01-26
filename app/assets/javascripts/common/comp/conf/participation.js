4e7bd75978b71149457b7b125f7dcc203c40be69
vit <none@none>
2013-01-26 18:13:11 +0000

    m

--- app/assets/javascripts/common/comp/conf/participation.js
+++ app/assets/javascripts/common/comp/conf/participation.js
@@ -33,7 +33,7 @@
 		var form_off_elm = $('[name=form_off]', cont);
 
 	//	var form_on = ParticipationFormOn( form_on_elm );
-		var form_on = ParticipationFormOn2( form_on_elm );
+		var form_on = ParticipationFormOn( form_on_elm );
 		var form_off = ParticipationFormOff( form_off_elm );
 
 		form_off.attach('want_register', function() {
@@ -78,24 +78,26 @@
 
 
 
-	var ParticipationFormOn2 = function(cont) {
+	var ParticipationFormOn = function(cont) {
 		var dict = new Dict( $('[name=dict]', cont) );
 		var form = $('form', cont);
-		function rulesCoauthors(name, err) {
-			if( this.get()['part_type'] != 'nonauthor' ) {
-				err('required');
-			}
-		}
-		function rulesDegree(name, err) {
-			if( this.get()['occupation'] == 'work' ) {
-				err('required');
-			}
+		function goPartTypeDependent(name) {
+			var flagEnabled = this.get()['part_type'] != 'nonauthor';
+			this.enableElement('coauthors', flagEnabled);
 		}
 		function goOccupationDependent(name) {
-	//		if( this.get()['occupation'] == 'work' ) {
-				this.enableElement('position', this.get()['occupation'] == 'work');
-			//	err('required');
-	//		}
+			var flagEnabled = this.get()['occupation'] == 'work';
+			this.enableElement('position', flagEnabled);
+			this.enableElement('rank', flagEnabled);
+			this.enableElement('degree', flagEnabled);
+		}
+		function goNationalityDependent(name) {
+			var flagEnabled = this.get()['nationality'] != 'ru';
+			this.enableElement('passport', flagEnabled);
+		}
+		function goHotelDependent(name) {
+			var flagEnabled = this.get()['hotel'] == 'hotel_myself';
+			this.enableElement('hotel_name', flagEnabled);
 		}
 
 		var formStruct = [
@@ -105,13 +107,13 @@
 			{name: 'fname', type: 'text', isvalid: 'notempty'},
 			{name: 'mname', type: 'text', isvalid: 'notempty'},
 			{name: 'birthdate', type: 'date', isvalid: 'notempty'},
-			{name: 'part_type', type: 'radio', isvalid: 'notempty'},
-			{name: 'coauthors', type: 'text', rules: rulesCoauthors},
+			{name: 'part_type', type: 'radio', isvalid: 'notempty', goDependent: goPartTypeDependent},
+			{name: 'coauthors', type: 'text'},
 			{name: 'occupation', type: 'radio', isvalid: 'notempty', goDependent: goOccupationDependent},
 			{name: 'organization', type: 'text', isvalid: 'notempty'},
-			{name: 'position', type: 'text', rules: rulesDegree},
-			{name: 'rank', type: 'text', rules: rulesDegree},
-			{name: 'degree', type: 'text', rules: rulesDegree},
+			{name: 'position', type: 'text'},
+			{name: 'rank', type: 'text'},
+			{name: 'degree', type: 'text'},
 			{name: 'responsibilities', type: 'text', isvalid: 'notempty'},
 			{name: 'org_country', type: 'text', isvalid: 'notempty'},
 			{name: 'org_city', type: 'text', isvalid: 'notempty'},
@@ -123,104 +125,11 @@
 			{name: 'mobile_phone', type: 'text'},
 			{name: 'fax', type: 'text'},
 			{name: 'email', type: 'text', isvalid: 'notempty'},
-			{name: 'nationality', type: 'text', isvalid: 'notempty'},
+			{name: 'nationality', type: 'text', isvalid: 'notempty', goDependent: goNationalityDependent},
 			{name: 'passport', type: 'text', isvalid: 'notempty'},
-			{name: 'hotel', type: 'radio', isvalid: 'notempty'},
+			{name: 'hotel', type: 'radio', isvalid: 'notempty', goDependent: goHotelDependent},
 			{name: 'hotel_name', type: 'text'},
 			{name: 'publish_agree', type: 'radio', isvalid: 'notempty'}
-		//	{name: '', type: 'text'},
-		];
-
-		function init(data) {
-			$('form[name=unregister_form]', cont).css('display', data ? 'block' : 'none');
-		//	di.clear();
-			if(data)
-				di.set(data);
-		}
-
-		var me = {
-			panel: cont,
-			init: init
-		};
-
-		var di = new JSComp.FormAccess2( form, formStruct );
-
-		$( "[name=form_save_btn]", form ).click(function(){
-			if( di.validate() ) {
-				var data = di.get();
-				me.notify('save_form', data);
-			} else {
-				alert( dict('msg_missing_required_fields') );
-			}
-		});
-		$( "[name=unreg_btn]", form ).click(function(){
-			if( confirm(dict('unregister_are_you_sure')) ) {
-				me.notify('drop_form');
-			}
-		});
-
-		Mixin.implement(me, Mixin.Observable);
-		Mixin.implement(me, PanelMixin);
-		return me;
-	}
-
-/*
-
-
-
-
-
-
-	var ParticipationFormOn = function(cont) {
-	//	alert(cont.attr('name'));
-		var dict = new Dict( $('[name=dict]', cont) );
-		var form = $('form', cont);
-		function rulesCoauthors(name, err) {
-			if( this.get()['part_type'] != 'nonauthor' ) {
-				err('required');
-			}
-		}
-		function rulesDegree(name, err) {
-			if( this.get()['occupation'] == 'work' ) {
-				err('required');
-			}
-		}
-		var formStruct = [
-			{name: 'gender', type: 'radio', rules: 'required'},
-			{name: 'person_title', type: 'list', rules: 'required'},
-			{name: 'lname', type: 'text', rules: 'required'},
-			{name: 'fname', type: 'text', rules: 'required'},
-			{name: 'mname', type: 'text', rules: 'required'},
-			{name: 'birthdate', type: 'date', rules: 'required'},
-			{name: 'part_type', type: 'radio', rules: 'required'},
-		//	{name: 'coauthors', type: 'text', rules: 'required'},
-			{name: 'coauthors', type: 'text', rules: rulesCoauthors},
-			{name: 'occupation', type: 'radio', rules: 'required'},
-			{name: 'organization', type: 'text', rules: 'required'},
-		//	{name: 'position', type: 'text', rules: 'required'},
-			{name: 'position', type: 'text', rules: rulesDegree},
-		//	{name: 'rank', type: 'text', rules: 'required'},
-			{name: 'rank', type: 'text', rules: rulesDegree},
-		//	{name: 'degree', type: 'text', rules: 'required'},
-			{name: 'degree', type: 'text', rules: rulesDegree},
-			{name: 'responsibilities', type: 'text', rules: 'required'},
-			{name: 'org_country', type: 'text', rules: 'required'},
-			{name: 'org_city', type: 'text', rules: 'required'},
-			{name: 'org_postcode', type: 'text', rules: 'required'},
-			{name: 'org_street', type: 'text', rules: 'required'},
-			{name: 'org_house', type: 'text', rules: 'required'},
-		//	{name: 'org_foreign', type: 'checkbox', rules: 'required'},
-			{name: 'org_foreign', type: 'checkbox'},
-			{name: 'phone', type: 'text', rules: 'required'},
-			{name: 'mobile_phone', type: 'text'},
-			{name: 'fax', type: 'text'},
-			{name: 'email', type: 'text', rules: 'required'},
-			{name: 'nationality', type: 'text', rules: 'required'},
-			{name: 'passport', type: 'text', rules: 'required'},
-			{name: 'hotel', type: 'radio', rules: 'required'},
-			{name: 'hotel_name', type: 'text'},
-			{name: 'publish_agree', type: 'radio', rules: 'required'}
-		//	{name: '', type: 'text'},
 		];
 
 		function init(data) {
@@ -236,37 +145,25 @@
 		};
 
 		var di = new JSComp.FormAccess( form, formStruct );
-	//	var di = new JSComp.FormAccess2( form, formStruct );
 
 		$( "[name=form_save_btn]", form ).click(function(){
-		//	confirm(dict('unregister_are_you_sure'));
-		//	var data = getData();
-
 			if( di.validate() ) {
 				var data = di.get();
-			//	alert(JSON.encode(data));
 				me.notify('save_form', data);
 			} else {
 				alert( dict('msg_missing_required_fields') );
 			}
 		});
-
 		$( "[name=unreg_btn]", form ).click(function(){
 			if( confirm(dict('unregister_are_you_sure')) ) {
 				me.notify('drop_form');
 			}
-		//	var data = getData();
-
-		//	alert('qwerq rtqrwtqwr');
 		});
 
 		Mixin.implement(me, Mixin.Observable);
 		Mixin.implement(me, PanelMixin);
 		return me;
 	}
-
-
-*/
 
 	var ParticipationFormOff = function(cont) {
 		var form = $('form', cont);
@@ -295,9 +192,7 @@
 
 	self.JSComp = self.JSComp || {};
 
-
-
-		self.JSComp.FormAccess2 = function( cont, info ) {
+		self.JSComp.FormAccess = function( cont, info ) {
 			var infoMap = {};
 			function initForm() {
 				$.each(info, function(k,v) {
@@ -432,135 +327,6 @@
 			return me;
 		}
 
-
-
-
-/*
-
-
-
-
-		self.JSComp.FormAccess = function( cont, info ) {
-			function initForm() {
-			//	$.each(formStruct, function(k,v) {
-				$.each(info, function(k,v) {
-					if(v.type=='date')
-						cont.find('[name='+v.name+']').datepicker({
-							"dateFormat": 'yy-mm-dd',
-							changeYear: true
-						});
-				});
-			}
-			function unmarkAllElements() {
-				$.each(info, function(k,v) {
-					markElement(v, false);
-				});
-			}
-			function markElement(v, flag) {
-				var elm = cont.find('[name='+v.name+']');
-				var len = elm.length;
-				if( len > 1 ) {
-					elm.each(function() {
-						var p = $(this).parent();
-						flag ? p.addClass('marked') : p.removeClass('marked');
-					});
-				} else {
-				//	elm.addClass('marked');
-					flag ? elm.addClass('marked') : elm.removeClass('marked');
-				}
-			}
-			function getElemData(v) {
-				var elm = cont.find('[name='+v.name+']');
-				var len = elm.length;
-				var rez;
-				if( len > 1 ) {
-					rez = null;
-					elm.each(function() {
-						v = $(this);
-						if( v.attr('checked') ) {
-							rez = v.val();
-						}
-					});
-				} else {
-					if( v.type=='checkbox' ) rez = elm.attr('checked') ? true : false;
-					else rez = elm.val();
-				}
-				//var rez = elm.length;
-				return rez;
-			}
-			function setElemData(v, d) {
-				var elm = cont.find('[name='+v.name+']');
-				var len = elm.length;
-				if( len > 1 ) {
-					elm.val([d]);
-		//			elm.each(function() {
-		//				v = $(this);
-		//				v.removeAttr('checked');
-		//				if( v.val()==d ) {
-		//					v.attr('checked', 'checked');
-		//				}
-		//			});
-				} else {
-					if( v.type=='checkbox' ) elm.attr('checked', d);
-					else elm.val(d);
-				}
-			}
-			function getData() {
-				var rez = {};
-				$.each(info, function(k,v) {
-					rez[v.name] = getElemData(v);
-				});
-				return rez;
-			}
-			function setData(data) {
-			//	alert('set');
-				$.each(info, function(k,v) {
-					setElemData(v, data[v.name]);
-				});
-			}
-			function validate() {
-				var err = [];
-				unmarkAllElements();
-
-				$.each(info, function(k,v) {
-					if( v.rules ) {
-						var val = getElemData(v);
-		//				alert( $.type(v.rules) );
-						if( $.type(v.rules)=='string' ) {
-							var rules = v.rules.split('|');
-							$.each(rules, function(k2,v2) {
-								switch( v2 ) {
-									case 'required':
-										if(!val) {
-											markElement(v, true);
-											err.push({name: v.name, rule: v2});
-										}
-								}
-							});
-						} else if( $.type(v.rules)=='function' ) {
-						//	v.rules.call(me, v.name, err);
-							v.rules.call(me, v.name, function(rule) {
-								if(!val) {
-									markElement(v, true);
-									err.push({name: v.name, rule: rule});
-								}
-							});
-						}
-					}
-				});
-		//		alert( JSON.stringify(err) );
-				return err.length == 0;
-			}
-			var me = {
-				get: getData,
-				set: setData,
-				validate: validate
-			};
-			initForm();
-			return me;
-		}
-
-*/
 
 }(jQuery));
 
