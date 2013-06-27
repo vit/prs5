81197f357e87fc421c77fd550d4e1d4931913187
vit <none@none>
2013-06-27 15:48:09 +0400

    changed model initialization function

--- config/initializers/appmodel_init.rb
+++ config/initializers/appmodel_init.rb
@@ -4,5 +4,6 @@ $:.unshift File.expand_path('../../../../', __FILE__)
 require 'prs_model/app'
 #require 'streamsheet/streamsheet'
 
-Coms::App.init( File.expand_path('../../prs_model_conf.yaml', __FILE__) )
+#Coms::App.init( File.expand_path('../../prs_model_conf.yaml', __FILE__) )
+Coms::App.init( File.expand_path('../../prs_model_conf.yaml', __FILE__) , Rails.env)
 
