766fef12d4cb7786879f1e8321fe524acf0b0fcf
vit <none@none>
2013-07-15 06:39:50 +0000

    m

--- app/controllers/adm/post_controller.rb
+++ app/controllers/adm/post_controller.rb
@@ -9,6 +9,15 @@ class Adm::PostController < ApplicationController
 		render (@appl.user.is_admin(@current_user[:user_id]) ? 'slots' : 'adm/main/norights')
 	end
 	def mailing
+		@mail_producers = @appl.post.producer.get_list
+		@mail_tasks = @appl.post.taskmgr.get_tasks_list
+		@mail_templates = @appl.post.get_templates nil
+		@confs_list = @appl.conf.get_confs_list.select { |c|
+			c['info'] && c['info']['title'] && (
+				c['info']['title']['en'] && c['info']['title']['en'].length>0 ||
+				c['info']['title']['ru'] && c['info']['title']['ru'].length>0
+			)
+		}
 		render (@appl.user.is_admin(@current_user[:user_id]) ? 'mailing' : 'adm/main/norights')
 	end
 end
