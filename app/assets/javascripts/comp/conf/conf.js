e5d19f2cbe60a75b47f02809df05d12a743baa59
vit <none@none>
2014-07-01 14:24:00 +0400

    m

--- app/assets/javascripts/comp/conf/conf.js
+++ app/assets/javascripts/comp/conf/conf.js
@@ -9,24 +9,24 @@ window.addEvent('domready',function() {
 				RPC.send('conf.get_conf_permissions', [me.id], function(result, error) {
 				//	alert( JSON.encode(result) );
 					if (result) me.permissions = result;
-//					RPC.send('conf.get_my_rights', [self.user.id, me.id], function(result, error) {
-//					//	alert( JSON.encode(result) );
-//						if (result) me.my_rights = result;
-//						me.fireEvent('init_ok');
-//					});
 					RPC.send('conf.get_my_rights', [self.user.id, me.id], function(result, error) {
 					//	alert( JSON.encode(result) );
 						if (result) me.my_rights = result;
-					//	me.fireEvent('init_ok');
-
-						RPC.send('conf.get_conf_info', [me.id], function(result, error) {
-						//	alert( JSON.encode(result) );
-				//			console.log( result );
-							if (result) me.info = result;
-							me.fireEvent('init_ok');
-						});
-
+						me.fireEvent('init_ok');
 					});
+	//				RPC.send('conf.get_my_rights', [self.user.id, me.id], function(result, error) {
+	//				//	alert( JSON.encode(result) );
+	//					if (result) me.my_rights = result;
+	//				//	me.fireEvent('init_ok');
+
+	//					RPC.send('conf.get_conf_info', [me.id], function(result, error) {
+	//					//	alert( JSON.encode(result) );
+	//			//			console.log( result );
+	//						if (result) me.info = result;
+	//						me.fireEvent('init_ok');
+	//					});
+
+	//				});
 				});
 			},
 			info: {},
